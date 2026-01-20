73a19eef75e6640eb98e4b2cbba74d70eaac728f
EMHaze <130314527+EMHaze@users.noreply.github.com>
2026-01-20 12:22:26 -0500

    adding buzzer code
    also the edge case needs to be fixed, should test

--- final_psa_code.py
+++ final_psa_code.py
@@ -1,6 +1,12 @@
 import board
 import digitalio
 import time
+import time, board, pwmio
+
+notes = [n * 2 for n in [330, 330]]
+tone = pwmio.PWMOut(board.GP15, variable_frequency=True)
+tone_duration = 0.2
+rest_duration = 0.2
 
 # Configure which GPIO pin the leds are connected to
 r_led = digitalio.DigitalInOut(board.GP16)
@@ -28,6 +34,22 @@ plant_button.pull = digitalio.Pull.UP
 # On startup, mode is set to None so that none of the lights are on
 mode = None
 
+# Buzzer functions 
+def play(freq, dur): #controls what sound the buzzer makes 
+    tone.frequency = freq
+    time.sleep(dur)
+    return
+
+
+def beep(notes): # when called allows the buzzer to sound 
+    for n in notes:
+            tone.duty_cycle = 32768
+            play(n, tone_duration)
+        
+            tone.duty_cycle = 0
+            time.sleep(rest_duration)
+    return 
+        
 # Flags for previous state of the BUTTON 
 prev_plant = True # Why are these true and not false? -> because we're using pull UP!!
 prev_people = True #True = 0V -> LEDs off
@@ -39,15 +61,22 @@ while True:
     if prev_people and not cur_people: # if state was people and people WAS pressed, <- tis is where the pull-up thing comes in
         if mode == "people":
             mode = None
+            beep(notes)
         else:
             mode = "people"
+            beep(notes)
 # HERE, WHY CARE IF PREV_PLANT? -> to track BUTTON state: mode tracks led state but not button state. 
 # this code cycles through 20 times a second, and we want the button to remember its state until we change it
     if prev_plant and not cur_plant: # if state was plant, and plant is pressed again
         if mode == "plant": # if the current mode is plant, make it None
             mode = None
+            beep(notes)
         else: # if the current mode in None, make it plant
             mode = "plant"
+            beep(notes)
+    if cur_people and cur_plant: 
+        continue 
+
 
     '''
     if plant button pressed:
@@ -71,7 +100,6 @@ while True:
         r_led.value = True
         b_led.value = True
         g_led.value = True
-
     # update previous states
     prev_people = cur_people 
     prev_plant = cur_plant 
